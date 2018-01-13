0f7a68fc1d528a0a99af87c300240e6551c193bb
zpf1995 <1970156609@qq.com>
2018-01-13 21:11:28 +0800

    last alter hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, make_response,redirect,abort
+from flask_script import Manager
 
 app=Flask(__name__)
+manager=Manager(app)
 @app.route('/')
 def index():
-    user_agent=request.headers.get('User-Agent')
-    return '<p>Your browser is {0}</p>'.format(user_agent)
+    # user_agent=request.headers.get('User-Agent')
+    # return '<p>Your browser is {0}</p>'.format(user_agent)
+    # response=make_response('<h1>take a cookie</h1>')
+    # response.set_cookie('answer','42')
+    # return response
+    return redirect('http://www.baidu.com')
 @app.route('/ss/<name>')
 def user(name):
+    if not name:
+        abort(405)
     return '<h1>Hello {0}</h1>'.format(name)
+@app.route('/404')
+def FOF():
+    return '<h2>Bad Request</h2>',404
 if __name__=='__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manager.run()
